6a7e607b0167dc1dfb381539989d81f5d50555e7
EnzoGaticaOdato <enzogaticaodato@gmail.com>
2023-05-09 14:47:13 -0700

    Header + Acerca de

--- ArgentinaPrograma-EnzoGaticaOdato/src/app/app.module.ts
+++ ArgentinaPrograma-EnzoGaticaOdato/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoArgentinaProgramaComponent } from './components/logo-argentina-programa/logo-argentina-programa.component';
+import { RedesComponent } from './components/redes/redes.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { AcercadeComponent } from './components/acercade/acercade.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoArgentinaProgramaComponent,
+    RedesComponent,
+    BannerComponent,
+    AcercadeComponent
   ],
   imports: [
     BrowserModule,

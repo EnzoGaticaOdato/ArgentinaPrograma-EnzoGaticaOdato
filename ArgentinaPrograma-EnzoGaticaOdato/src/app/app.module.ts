c020b9e8dd53bbfe20cc88b47d0bc1e585c49525
EnzoGaticaOdato <enzogaticaodato@gmail.com>
2023-05-09 15:07:48 -0700

    Educacion y Experiencial laboral (falta actualizar)

--- ArgentinaPrograma-EnzoGaticaOdato/src/app/app.module.ts
+++ ArgentinaPrograma-EnzoGaticaOdato/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { LogoArgentinaProgramaComponent } from './components/logo-argentina-prog
 import { RedesComponent } from './components/redes/redes.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercadeComponent } from './components/acercade/acercade.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,9 @@ import { AcercadeComponent } from './components/acercade/acercade.component';
     LogoArgentinaProgramaComponent,
     RedesComponent,
     BannerComponent,
-    AcercadeComponent
+    AcercadeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,

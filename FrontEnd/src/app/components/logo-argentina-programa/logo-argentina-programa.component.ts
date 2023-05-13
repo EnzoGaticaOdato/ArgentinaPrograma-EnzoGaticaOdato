7f771d554f1cd30e149d165b9c381af11e068352
EnzoGaticaOdato <enzogaticaodato@gmail.com>
2023-05-12 20:16:32 -0700

    Cargada base de datos y login en HTML

--- FrontEnd/src/app/components/logo-argentina-programa/logo-argentina-programa.component.ts
+++ FrontEnd/src/app/components/logo-argentina-programa/logo-argentina-programa.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-logo-argentina-programa',
   templateUrl: './logo-argentina-programa.component.html',
   styleUrls: ['./logo-argentina-programa.component.css']
 })
-export class LogoArgentinaProgramaComponent {
+export class LogoArgentinaProgramaComponent implements OnInit{
+
+  constructor(private router:Router){}
+
+  ngOnInit():void{
+    
+  }
+
+  login(){
+    this.router.navigate(['/login'])
+  }
 
 }
